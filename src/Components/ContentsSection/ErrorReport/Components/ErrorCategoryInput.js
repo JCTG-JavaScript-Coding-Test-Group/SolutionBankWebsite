86948507ca838248a8fbd8bfc71f3011536030bb
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-06-30 04:17:14 +0900

    fix: 오류 유형 변경시 input 남아있는 문제 해결

--- src/Components/ContentsSection/ErrorReport/Components/ErrorCategoryInput.js
+++ src/Components/ContentsSection/ErrorReport/Components/ErrorCategoryInput.js
@@ -1,14 +1,17 @@
 export default function ErrorCategoryInput() {
+  function resetInput() {
+    Array.from(document.querySelectorAll("input, textarea")).forEach((input) => (input.value = ""));
+  }
   return (
     <div className="errorCategoryinput">
       <span>오류 유형: </span>
-      <input type="radio" id="errorCategory0" name="errorCategory" value="email" />
+      <input type="radio" id="errorCategory0" name="errorCategory" onClick={resetInput} />
       <label htmlFor="errorCategory0">정답 통과가 안돼요</label>
 
-      <input type="radio" id="errorCategory1" name="errorCategory" value="phone" />
+      <input type="radio" id="errorCategory1" name="errorCategory" onClick={resetInput} />
       <label htmlFor="errorCategory1">코드 복사가 안돼요</label>
 
-      <input type="radio" id="errorCategory2" name="errorCategory" value="mail" />
+      <input type="radio" id="errorCategory2" name="errorCategory" onClick={resetInput} />
       <label htmlFor="errorCategory2">기타</label>
     </div>
   );
